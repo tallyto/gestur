9bfb7e7b0a6c61beca49594b774212c5d1a780eb
tallyto <rodrigues.tallyto@hotmail.com>
2024-04-20 14:57:06 -0300

    feat: alterar entidade item_pedido para item_venda

--- src/main/java/com/tallyto/gestur/repository/ItemVendaRepository.java
+++ src/main/java/com/tallyto/gestur/repository/ItemVendaRepository.java
@@ -1,2 +1,7 @@
-package com.tallyto.gestur.repository;public class ItemVendaRepository {
+package com.tallyto.gestur.repository;
+
+import com.tallyto.gestur.model.ItemVenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemVendaRepository extends JpaRepository<ItemVenda, Long>{
 }
